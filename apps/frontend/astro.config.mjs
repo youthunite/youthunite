e53a0b1e12bfc696e0f9c953112985b244a2ce8d
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2025-08-26 23:43:00 +0200

    fix: site url

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -9,7 +9,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://youthunite.onlinec',
+  site: 'https://youthunite.online',
   integrations: [svelte()],
   output: 'server',
 
