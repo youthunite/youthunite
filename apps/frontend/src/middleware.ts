a22fb8fff9a0aab5444b502fbf9c4f8c33615dbe
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-08-27 00:30:29 +0200

    feat: protect register events route

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -1,6 +1,7 @@
 import { defineMiddleware } from 'astro:middleware';
 import { getServerUser, getTokenFromCookie } from './lib/server/auth';
 
+// TODO: THIS NEEDS A REFACTOR. ASAP.
 export const onRequest = defineMiddleware(async (context, next) => {
   const { url, request, redirect } = context;
   
@@ -20,6 +21,21 @@ export const onRequest = defineMiddleware(async (context, next) => {
       return redirect('/login?redirect=' + encodeURIComponent(url.pathname));
     }
     
+    // @ts-ignore just uh
+    context.locals.user = user;
+  }
+
+  if (url.pathname === '/events/register') {
+    const cookieHeader = request.headers.get('cookie');
+    const token = getTokenFromCookie(cookieHeader);
+
+    if (!token) {
+      return redirect('/login?redirect=' + encodeURIComponent(url.pathname));
+    }
+    const user = await getServerUser(token);
+    if (!user) {
+      return redirect('/login?redirect=' + encodeURIComponent(url.pathname));
+    }
     // @ts-ignore just uh
     context.locals.user = user;
   }
